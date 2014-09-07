664c98d97d9b693f7044f0bcb06e9d573d5d7e69
lucaracca <luca.racca@gmail.com>
2014-09-07 17:07:48 +0200

    log file per upload in cartelle condivise

--- PoliBox/src/main/java/it/polito/ai/polibox/controller/UploadController.java
+++ PoliBox/src/main/java/it/polito/ai/polibox/controller/UploadController.java
@@ -37,23 +37,33 @@ public class UploadController {
 		System.out.println(path);
 		String[] pathElements = path.replace("%20", " ").split("/");
 		String pathDir = new String();
+		Utente owner = new Utente();
+		Condivisione condivisione = new Condivisione ();
+		String pathUrl = new String();
+		String pathLog = new String();
+		String condName = new String();
 		if (cond == 1) {
 			// creazione in una cartella condivisa
-			Condivisione condivisione = condivisioneDAO.getCondivisioneWithoutTrans((Long) session.getAttribute("cId"));
-			Utente owner = utenteDAO.getUtenteWithoutTrans(condivisione.getOwnerId());
-			pathDir = owner.getHome_dir();
+			condivisione = condivisioneDAO.getCondivisioneWithoutTrans((Long) session.getAttribute("cId"));
+			owner = utenteDAO.getUtenteWithoutTrans(condivisione.getOwnerId());
+			pathDir = condivisione.getDirPath();
+			condName = pathElements[5];
+			for (int i=6; i<pathElements.length; i++) {
+				pathUrl += "\\" + pathElements[i];
+				pathLog += "/"+pathElements[i];
+			}
+			pathDir += pathUrl;
 		} else {
 			pathDir = utente.getHome_dir();
-		}
-		String pathUrl = new String();
-		for (int i=5; i<pathElements.length; i++) {
-			if (i==5) {
-				pathUrl += pathElements[i];
-			} else {
-				pathUrl += "\\" + pathElements[i];
+			for (int i=5; i<pathElements.length; i++) {
+				if (i==5) {
+					pathUrl += pathElements[i];
+				} else {
+					pathUrl += "\\" + pathElements[i];
+				}
 			}
+			pathDir += "\\Polibox\\" + pathUrl;
 		}
-		pathDir += "\\Polibox\\" + pathUrl;
 		List<String> fileNames = new ArrayList<String>();
 		  
 		if (uploadedFiles == null || uploadedFiles.size() == 0 || uploadedFiles.get(0).getOriginalFilename() == "") {
@@ -78,7 +88,20 @@ public class UploadController {
 			File dest = new File(pathDir + "\\" + fileName);
 			try {
 				file.transferTo(dest);
-				log.addLine(utente.getId(), "NF", path+"/"+fileName, 0);
+				if(cond==1) {
+					Log owner_log = new Log(owner.getHome_dir());
+					String[] p = condivisione.getDirPath().split("\\\\");
+					int flag=0;
+					String pp = new String("http://localhost:8080/ai/home");
+					for (int i=0;i<p.length;i++) {
+						if (flag==1) pp += "/"+p[i];
+						if (p[i].equals("Polibox")) flag=1;
+					}
+					log.addLine(utente.getId(), "NF","http://localhost:8080/ai/home/"+p[p.length-1]+pathLog+"/"+fileName , 0, owner.getId());
+					owner_log.addLine(owner.getId(), "NF",pp+pathLog+"/"+fileName , 0, utente.getId());
+				} else {
+					log.addLine(utente.getId(), "NF",path+"/"+fileName , 0);
+				}
 			} catch (IllegalStateException ise) {
 				ise.printStackTrace();
 				redirectAttrs.addFlashAttribute("utente", utente);
@@ -127,7 +150,7 @@ public class UploadController {
 			return "redirect:home";
 		}
 		if (cond == 1) {
-			return "redirect:Home/" + pathUrl.replace("\\", "/");
+			return "redirect:Home/" + condName +pathLog;
 		} else {
 			return "redirect:home/" + pathUrl.replace("\\", "/");
 		}
